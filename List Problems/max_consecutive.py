356df3dccfd78bece6cc07c9275666d4ccca7471
Rajat <rajatbr13@gmail.com>
2025-07-31 14:33:41 +0530

    Added Rearrange array elements by signs

--- List Problems/max_consecutive.py
+++ List Problems/max_consecutive.py
@@ -1,16 +1,16 @@
 #Given a binary array, find the max consecutive 1s in the array
 nums = [1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0]
-def MaxConsecutiveOnes(nums): # TC is O(N) SC is also O(1) this is optimal
+def maxx(nums): # TC is O(N) SC is also O(1) this is optimal
     count = 0
     max_count = 0 
     for i in nums:
         if i == 1:
             count +=1
-            if count > max_count:
+            if count > max_count: # max count increase only if count > max count
                 max_count = count
         elif i == 0:
             count = 0
 
     return (max_count)
 
-print(MaxConsecutiveOnes(nums))
+print(maxx(nums))
