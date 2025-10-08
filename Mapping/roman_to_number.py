868019f5e6803654e84ac93667e8ae3436f533e3
Rajat <rajatbr13@gmail.com>
2025-10-08 22:00:49 +0530

    Added Integer to Roman

--- Mapping/roman_to_number.py
+++ Mapping/roman_to_number.py
@@ -1,4 +1,4 @@
-#Convert Roman numbera to numeric valuea
+#Convert Roman number to numeric value [Leetcode : Easy]
 # I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000
 
 dict = {
@@ -11,7 +11,7 @@ dict = {
     "M" : 1000
 }
 
-a = ("MXLVIII")
+a = ("MCCLVII")
 s = a[::-1] # Reverse the string and traverse in back order
 ans = dict[s[0]] #start with the first (last) value
 
